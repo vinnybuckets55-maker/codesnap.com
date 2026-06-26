8d82eff4005537edae270e85ea3224bcfa6413b4
Vinny Buckets <vinnybuckets55@gmail.com>
2026-06-26 13:49:06 -0600

    Remove initial setup code from create-reel.js

--- create-reel.js
+++ create-reel.js
@@ -1,6 +1,1 @@
-// --- FILE LOAD IMPORTER ENGINE ---
-const triggerPickerBtn = document.getElementById('triggerVideoPickerBtn');
-const videoPicker = document.getElementById('reelVideoPicker');
-const localPreview = document.getElementById('localReelPreview');
-const previewPlaceholder = document.getElementById('previewPlaceholder');
-const publishReelBtn
+
