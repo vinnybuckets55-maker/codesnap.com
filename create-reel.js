a8efa56b5554f3bb448ec38d2a2c25603b75dcc5
Vinny Buckets <vinnybuckets55@gmail.com>
2026-06-26 13:48:17 -0600

    Add initial setup for reel creation functionality

---
+++ create-reel.js
@@ -0,0 +1,6 @@
+// --- FILE LOAD IMPORTER ENGINE ---
+const triggerPickerBtn = document.getElementById('triggerVideoPickerBtn');
+const videoPicker = document.getElementById('reelVideoPicker');
+const localPreview = document.getElementById('localReelPreview');
+const previewPlaceholder = document.getElementById('previewPlaceholder');
+const publishReelBtn
